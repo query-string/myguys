826978bd7dc5a9ce024ff3596388900eb9c40bd7
query-string <atimofeev@reactant.ru>
2015-07-15 11:08:29 +0700

    Show log notice about `IMGX_URL` variable definition necessity.

--- app/models/image.rb
+++ app/models/image.rb
@@ -8,6 +8,7 @@ class Image < ActiveRecord::Base
   scope :newst, -> { order(created_at: :desc) }
 
   def imgx_url
+    Rails.logger.debug "Please, change the value of a ENV[\"IMGX_URL\"] valriable (it must contain an actual S3 domain name)" if ENV.fetch('IMGX_URL').nil? || ENV.fetch('IMGX_URL') == "XXX"
     imgx_uri = URI.parse(ENV.fetch('IMGX_URL'))
     imgx_uri.path = File.join(imgx_uri.path, image_path)
     imgx_uri.to_s
