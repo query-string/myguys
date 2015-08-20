34670ab0176dfd0c49f0db1213dd1854911501e0
query-string <atimofeev@reactant.ru>
2015-08-20 20:41:03 +0700

    Extract all client related methods to `SlackBot::Realtime` class

--- lib/slack_bot.rb
+++ lib/slack_bot.rb
@@ -1,63 +1,43 @@
 require "slack_bot/environment"
+require "slack_bot/realtime"
 require "slack_bot/listener"
 require "slack_bot/public_listener"
 require "slack_bot/private_listener"
 require "slack_bot/message_parser"
 
-# @TODO: Extract client related methods to client class
 # @TODO: User liteners as a message cleaners
 # @TODO: Message parser should only parse message, but not define a destination (?)
 
 class SlackBot
-  include SlackBot::Environment
-  attr_reader :attributes, :data
+  attr_reader :attributes, :data, :realtime, :target_channel
 
   def initialize(target_channel = "general")
+    @realtime       = SlackBot::Realtime.new
     @target_channel = target_channel
   end
 
   def start
-    client.on :message do |data|
+    realtime.client.on :message do |data|
       @data = data.to_hashugar
       populate_attributes
       listen
     end
-    client.start
+    realtime.client.start
   end
 
   private
 
-  def bot_user
-    client_response.self
-  end
-
-  def channel_ids
-    client_response.channels.map(&:id)
-  end
-
-  def im_list
-    client_response.ims
-  end
-
   def message_type
-    channel_ids.include?(data.channel) ? "public" : "private"
-  end
-
-  def client
-    @client ||= Slack.realtime
-  end
-
-  def client_response
-    @client_response ||= client.response.to_hashugar
+    realtime.channel_ids.include?(data.channel) ? "public" : "private"
   end
 
   def populate_attributes
     @attributes = {
-      client: client,
-      response: client_response,
+      client: realtime,
+      response: realtime.response,
       data: data,
-      bot_user: bot_user,
-      im_list: im_list,
+      bot_user: realtime.bot,
+      im_list: realtime.ims,
       target_channel: target_channel
     }.to_hashugar
   end
---
+++ lib/slack_bot/realtime.rb
@@ -0,0 +1,25 @@
+class SlackBot
+  class Realtime
+    attr_reader :client, :response
+
+    def initialize
+      @client ||= Slack.realtime
+    end
+
+    def response
+      @response ||= client.response.to_hashugar
+    end
+
+    def bot
+      response.self
+    end
+
+    def ims
+      response.ims
+    end
+
+    def channel_ids
+      client_response.channels.map(&:id)
+    end
+  end
+end
