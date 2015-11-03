a7d60e80217aa13a7a4bdf398d4e50ba0a234069
Alex Timofeev <atimofeev@reactant.ru>
2015-11-03 17:50:50 +0700

    Wrap `Slash` handler event attributes to appropriate methods

--- lib/slack_bot/handlers/slash.rb
+++ lib/slack_bot/handlers/slash.rb
@@ -2,7 +2,15 @@ class SlackBot
   module Handlers
     class Slash < SlackBot::Handlers::Base
       def proper_target_defined?
-        false
+        true
+      end
+
+      def message
+        event.text
+      end
+
+      def source
+        event.channel_id
       end
     end
   end
