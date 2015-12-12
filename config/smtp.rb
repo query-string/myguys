462df96623ffe0294440c4faf1adede6a9681992
Alex Timofeev <atimofeev@reactant.ru>
2015-12-12 16:59:27 +0700

    Fix syntax error

--- config/smtp.rb
+++ config/smtp.rb
@@ -1,7 +1,7 @@
 SMTP_SETTINGS = {
   address: ENV["SMTP_ADDRESS"], # example: "smtp.sendgrid.net"
   authentication: :plain,
-  domain: ENV["SMTP_DOMAIN"] # example: "this-app.com"
+  domain: ENV["SMTP_DOMAIN"], # example: "this-app.com"
   enable_starttls_auto: true,
   password: ENV["SMTP_PASSWORD"],
   port: "587",
