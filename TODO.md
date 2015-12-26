7c7cbd51a181da1f9abafd57f49adb4435366f2c
Alex Timofeev <atimofeev@reactant.ru>
2015-12-26 13:45:22 +0700

    Display help commands

--- TODO.md
+++ TODO.md
@@ -1,31 +1,32 @@
 # Myguys
 
+## Bugs
+
 ## Slack integration
-- [ ] Slack-it (Web UI):
-    - [ ] Group photo
-- [ ] Bot channel:
-    - [ ] Group photo (@higuys: show me/us @channel)
-- [ ] Bot PM: (show me @alex)
-    - [ ] Group photo (show me/us @channel)
-- [ ] Slash commands:
-    - [ ] Group photo (/higuys show me/us @channel)
+
+- [ ] Display commands:
+    - [ ] Private
+    - [ ] Public
+    - [x] Slash
+
 - [ ] Reminders:
     - [ ] Ping channel if nobody is active more than X hours
     - [ ] Ping channel if only one person is active for certain emount of time
     - [ ] Ping @username if he has never seen in hg
+
+- [ ] Group photo (@higuys: show me/us @channel):
+    - [ ] Private
+    - [ ] Public
+    - [ ] Slash
+
 - [ ] Specs
 - [ ] Favorite photo
 
 ## Common tweaks
-- [ ] Extract Airbreak to ENV
+- [x] Extract Airbreak to ENV
 - [ ] Pusher listens for status changes too
 - [ ] Changeable header colors
 - [ ] Personal domain for a wall
 - [ ] Use memoization
 
-## Archive
-
-- [ ] Bot commands:
-    - [ ] Ask about help
-- [ ] Slash commands:
-    - [ ] Ask about help
+## Public photo archive
--- app/controllers/api/slack_controller.rb
+++ app/controllers/api/slack_controller.rb
@@ -1,5 +1,11 @@
 module Api
   class SlackController < BaseController
+    COMMANDS_ALLOWED = %w(help show)
+    COMMANDS_HELP    = [
+      "`/higuys show us @username` – shows the latest user photo in public channel",
+      "`/higuys show me @username` – sends the latest user photo to PM"
+    ]
+
     def create
       image = Image.find(params[:image_id])
       SlackPostPhoto.execute "#general", image
@@ -8,17 +14,24 @@ module Api
     end
 
     def slash_command
-      if params[:token].present?
-        SlackBot::Notifier.new("slash_notification", {
-          text:       params[:text],
-          user_name:  params[:user_name],
-          user_id:    params[:user_id],
-          channel:    params[:channel_name],
-          channel_id: params[:channel_id]
-        }).notify
+      message = if params[:token].present? && COMMANDS_ALLOWED.find { |e| /#{params[:text]}/ =~ e }
+        if params[:text] == "help"
+          COMMANDS_HELP.join("\n")
+        else
+          SlackBot::Notifier.new("slash_notification", {
+            text:       params[:text],
+            user_name:  params[:user_name],
+            user_id:    params[:user_id],
+            channel:    params[:channel_name],
+            channel_id: params[:channel_id]
+          }).notify
+          ":ok_hand: Processing..."
+        end
+      else
+        ":thinking_face: Sorry, but this command is not allowed. Could you check `/higuys help` first?"
       end
 
-      render text: ":ok_hand:"
+      render text: message
     end
   end
 end
